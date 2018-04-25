8af2160241451257d58f1fe577c52b313ffebef0
rafrga1718 <32572290+rafrga1718@users.noreply.github.com>
2018-04-25 20:49:39 +0200

    commit 1
    commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades 
+Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: El teu nom i cognom
+Author URI: L'adreça URL del teu compte de Github
+License: GNU GPLv3 
+*/ 
+?>
