fccb773db64f322629f09490de6d38a0bad8cd8f
rafrga1718 <32572290+rafrga1718@users.noreply.github.com>
2018-04-25 21:03:28 +0200

    Commit 2

--- peupagina.php
+++ peupagina.php
@@ -1,11 +1,22 @@
 <?php
 /*
-Plugin Name: Peu de pàgina per defecte per a noves entrades 
-Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
+Plugin Name: Peu de pàgina per defecte per noves entrades
+Plugin URI: 
+Escriu l'adreça URL completa del teu plugin dins de Github
 Description: Crea peus de pàgina per defecte per a noves entrades
 Version: 0.1
-Author: El teu nom i cognom
-Author URI: L'adreça URL del teu compte de Github
-License: GNU GPLv3 
-*/ 
+Author: 
+El teu nom i cognom
+Author URI: 
+L'adreça URL del teu compte de Github
+License: GNU GPLv3
+*/
+function admin_menu_peu_pagina(){ 
+$titolPagina="Peu de pàgina per defecte";
+$titolEntrada="Peu de pàgina per defecte";
+$role="administrator";
+$id="peu_pagina_per_defecte";
+add_menu_page($titolPagina,$titolEntrada,$role,$id); ;
+}
+add_action('admin_menu','admin_menu_peu_pagina');
 ?>
